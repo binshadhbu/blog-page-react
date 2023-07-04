7bda68ed49cb3a4e51dc5b59e0f509c0a7ffb1f9
binshadhbu <binshadh_b210517cs@nitc.ac.in>
2023-07-04 11:51:07 +0530

    connected to redux and replaced with state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,25 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Pokeball from "../pokeball.png";
+import { connect } from "react-redux";
 
 class Home extends Component {
-  state = {
-    posts: []
-  }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts/')
-      .then(res => {
-        console.log(res);
-        this.setState({
-          posts: res.data.slice(0,10)
-        });
-      })
-  }
-  render(){
-    const { posts } = this.state
+  render() {
+    console.log(this.props);
+    const { posts } = this.props;
     const postList = posts.length ? (
-      posts.map(post => {
+      posts.map((post) => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="A pokeball" />
             <div className="card-content">
-              <Link to={'/' + post.id}>
+              <Link to={"/posts/" + post.id}>
                 <span className="card-title red-text">{post.title}</span>
               </Link>
               <p>{post.body}</p>
             </div>
           </div>
-        )
+        );
       })
     ) : (
       <div className="center">No posts to show</div>
@@ -41,8 +32,13 @@ class Home extends Component {
           {postList}
         </div>
       </div>
-    )
+    );
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+  };
+};
 
-export default Home
+export default connect(mapStateToProps)(Home);
